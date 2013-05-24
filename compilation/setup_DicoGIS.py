cb73121565d017f2fbfe5e2a7c37118cb29e10e2
Julien <julien.moura@gmail.com>
2013-05-24 13:52:47 -0500

    configuring script for compilation and renaming

--- compilation/setup_DicoGIS.py
+++ compilation/setup_DicoGIS.py
@@ -2,33 +2,32 @@
 import py2exe
 import os
 
+# custom modules
+from modules import *
+
 mfcdir = r'C:\Python27\Lib\site-packages\pythonwin'
 mfcfiles = [os.path.join(mfcdir, i) for i in ["mfc90.dll", "mfc90u.dll", "mfcm90.dll", "mfcm90u.dll", "Microsoft.VC90.MFC.manifest"]]
 
-includes = ["encodings",
-            "encodings.latin_1", "encodings.utf_8"]
-
 py2exe_options = dict(
-                      excludes=['_ssl',  # Exclude _ssl
-                                'pyreadline', 'doctest', 'email',
-                                'optparse'],  # Exclude standard library
-                      includes = includes,
                       dll_excludes = ['MSVCP90.dll'],
                       compressed=True,  # Compress library.zip
                       optimize = 2,
+                      dist_dir = 'DicoGIS'
                       )
 
-setup(name="Dico Shapes",
-      version="1.3",
-      description=u"Création d'un dictionnaire au format Excel (2003) des \
-                    shapefiles d'une arborescence",
+setup(name="DicoGIS",
+      version="1.5",
+      description=u"Dynamic dictionary of geographic datas",
       author="Julien Moura",
+      author_mail = "julien.moura@gmail.com",
+      url = "https://github.com/Guts/Solinette",
       license="license GPL v3.0",
+      data_files = [("", ["setting.xml"]),("", ["DicoGIS.ico"]),("img", ["img/DicoGIS_logo.gif"]),("documentacion",["documentation/DicoGIS_Manual_ES.docx"])],
       data_files=[("Microsoft.VC90.MFC", mfcfiles)],
       options={'py2exe': py2exe_options},
       windows = [
             {
-            "script": "DicoShapes_fr.py",              # script
+            "script": "DicoGIS.py",              # script
             "icon_resources": [(1, "DicoGIS.ico")]     # Icone
             }
                 ]
