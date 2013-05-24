1ee3f2c550088996dc3f800107e4c869af6e9796
Julien <julien.moura@gmail.com>
2013-05-24 15:37:23 -0500

    updated: script to compilation with py2exe

--- compilation/setup_DicoGIS.py
+++ compilation/setup_DicoGIS.py
@@ -5,11 +5,7 @@ import os
 # custom modules
 from modules import *
 
-mfcdir = r'C:\Python27\Lib\site-packages\pythonwin'
-mfcfiles = [os.path.join(mfcdir, i) for i in ["mfc90.dll", "mfc90u.dll", "mfcm90.dll", "mfcm90u.dll", "Microsoft.VC90.MFC.manifest"]]
-
 py2exe_options = dict(
-                      dll_excludes = ['MSVCP90.dll'],
                       compressed=True,  # Compress library.zip
                       optimize = 2,
                       dist_dir = 'DicoGIS'
@@ -22,8 +18,7 @@ setup(name="DicoGIS",
       author_mail = "julien.moura@gmail.com",
       url = "https://github.com/Guts/Solinette",
       license="license GPL v3.0",
-      data_files = [("", ["setting.xml"]),("", ["DicoGIS.ico"]),("img", ["img/DicoGIS_logo.gif"]),("documentacion",["documentation/DicoGIS_Manual_ES.docx"])],
-      data_files=[("Microsoft.VC90.MFC", mfcfiles)],
+      data_files = [("locale", ["locale/lang_EN.xml", "locale/lang_ES.xml", "locale/lang_FR.xml"]),("", ["settings.xml"]),("", ["DicoGIS.ico"]),("img", ["img/DicoGIS_logo.gif"]),("documentation",["documentation/DicoGIS_Manual_ES.docx"])],
       options={'py2exe': py2exe_options},
       windows = [
             {
@@ -32,3 +27,6 @@ setup(name="DicoGIS",
             }
                 ]
     )
+
+
+
