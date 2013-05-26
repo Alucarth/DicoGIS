4a8a3649282a1862d70c0302ce7b073bda860609
Guts <julien.moura@gmail.com>
2013-05-26 17:30:21 -0500

    improved: compilation script

--- compilation/setup_DicoGIS.py
+++ compilation/setup_DicoGIS.py
@@ -16,14 +16,23 @@ setup(name="DicoGIS",
       description=u"Dynamic dictionary of geographic datas",
       author="Julien Moura",
       author_mail = "julien.moura@gmail.com",
-      url = "https://github.com/Guts/Solinette",
+      url = "https://github.com/Guts/DicoGIS",
       license="license GPL v3.0",
-      data_files = [("locale", ["locale/lang_EN.xml", "locale/lang_ES.xml", "locale/lang_FR.xml"]),("", ["settings.xml"]),("", ["DicoGIS.ico"]),("img", ["img/DicoGIS_logo.gif"]),("documentation",["documentation/DicoGIS_Manual_ES.docx"])],
+      data_files = [("locale", ["locale/lang_EN.xml",
+                                "locale/lang_ES.xml",
+                                "locale/lang_FR.xml"]),
+                    ("", ["settings.xml"]),
+                    ("", ["DicoGIS.ico"]),
+                    ("img", ["img/DicoGIS_logo.gif"]),
+                    ("doc",["documentation/DicoGIS_Manual_ES.pdf",
+                            "documentation/README.HTML",
+                            "documentation/DicoGIS_GitHub",
+                            "documentation/DicoGIS_TechnicalDetails.html"])],
       options={'py2exe': py2exe_options},
       windows = [
             {
-            "script": "DicoGIS.py",              # script
-            "icon_resources": [(1, "DicoGIS.ico")]     # Icone
+            "script": "DicoGIS.py",                     # script
+            "icon_resources": [(1, "DicoGIS.ico")]      # Icone
             }
                 ]
     )
