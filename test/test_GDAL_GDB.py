e0ab96f9c2e3b3ba1a8aebcb292d91ad83355ddd
Julien <julien.moura@gmail.com>
2014-07-29 00:15:43 +0200

    diving into Esri FileGDB

--- test/test_GDAL_GDB.py
+++ test/test_GDAL_GDB.py
@@ -29,8 +29,12 @@ from collections import OrderedDict as OD
 # 3rd party libraries
 from osgeo import gdal    # handler for raster spatial files
 from osgeo import ogr
+
+
 from gdalconst import *
 gdal.AllRegister()
+ogr.UseExceptions()
+gdal.UseExceptions()
 
 ################################################################################
 ########### Classes #############
@@ -47,30 +51,52 @@ class Read_GDB_f():
 class Read_GDB_o():
     def __init__(self, gdb):
         """ """
-        print "\Let's play with OGR OpenGDB driver".upper()
+        print "\nLet's play with OGR OpenGDB driver".upper()
         print(dir(gdb))
-        print(gdb.GetLayerCount())
+        # print(gdb.GetStyleTable())
+        print("{0} layers found into.".format(gdb.GetLayerCount()))
         for index in range(gdb.GetLayerCount()):
+            # global information about each layer
             layer = gdb.GetLayerByIndex(index)
-            print(layer.GetName())
-            print(layer.GetExtent())
-            print(layer.GetFIDColumn())
-            print(layer.GetFeatureCount())
-            print(layer.GetGeomType())
-            print(layer.GetGeometryColumn())
+            print("\nLayer: {0}".format(layer.GetName()))
+            print("Xmin = {0} - Xmax = {1} \n\
+Ymin = {2} - Ymax = {3}".format(layer.GetExtent()[0],
+                                                   layer.GetExtent()[1],
+                                                   layer.GetExtent()[2],
+                                                   layer.GetExtent()[3]))
+            print("Geometry column name: {0}".format(layer.GetFIDColumn()))
+            print("# features: {0}".format(layer.GetFeatureCount()))
+            print("Geometry type: {0}".format(layer.GetGeomType()))
+            # print("{0}".format(layer.GetGeometryColumn()))
+            
+            # fields information about each layer
             layer_def = layer.GetLayerDefn()
+            print("# fields: {0}".format(layer_def.GetFieldCount()))
             print(dir(layer_def))
-            print(layer_def.GetFieldCount())
-            # print(layer_def.GetFieldDefn())
-            print(layer_def.GetGeomFieldCount())
+            print("# geometry fields: {0}".format(layer_def.GetGeomFieldCount()))
             # print(layer_def.GetGeomFieldDefn())
             print(layer_def.GetGeomType())
             # print(layer.GetName())
-            print(dir(layer))
+            print(help(layer_def.GetFieldDefn))
             style_table = layer.GetStyleTable()
             print(style_table)
-            
 
+            for fdidx in range(0, layer_def.GetFieldCount()-1):
+                field = layer_def.GetFieldDefn(fdidx)
+                print("\n= Field: {0}".format(field.GetName()))
+                print("== Type: {0}".format(field.GetTypeName()))
+                # print("== Type Name: {0}".format(field.GetFieldTypeName()))
+                print("== Name ref: {0}".format(field.GetNameRef()))
+                print("== Justify: {0}".format(field.GetJustify()))
+                print("== Precision: {0}".format(field.precision))
+                print("== Length: {0}".format(field.width))
+            
+            # end of fields loop
+            print(dir(field))
+
+        # end of function
+        print("\n")
+        print(dir(layer_def))
 
 
 ################################################################################
@@ -86,14 +112,14 @@ if __name__ == '__main__':
     gdb = r'..\test\datatest\GDB_Test.gdb'
 
     # OGR: FileGDB (see: http://www.gdal.org/drv_filegdb.html)
-    try:
-        dr_gdb_f = ogr.GetDriverByName("FileGDB")
-        gdb = dr_gdb_f.Open(gdb, 0)
-        print("OGR driver FileGDB: OK!")
-        Read_GDB_f(gdb)
-    except AttributeError:
-        print("FileGeodatabases can't be read by this driver: {0}.\
-        See: http://www.esri.com/apps/products/download/#File_Geodatabase_API_1.3".format("FileGDB"))
+    # try:
+    #     dr_gdb_f = ogr.GetDriverByName("FileGDB")
+    # except AttributeError:
+    #     print("FileGeodatabases can't be read by this driver: {0}.\
+    #     See: http://www.esri.com/apps/products/download/#File_Geodatabase_API_1.3".format("FileGDB"))
+    # else:
+    #     print("OGR driver FileGDB: OK!")
+    #     Read_GDB_f(gdb)
 
     # OGR: OpenFileGDB (see: http://www.gdal.org/drv_openfilegdb.html)
     try:
@@ -101,7 +127,8 @@ if __name__ == '__main__':
         gdb = dr_gdb_o.Open(gdb, 0)
         print("OGR driver OpenFileGDB: OK!")
         Read_GDB_o(gdb)
-    except:
+    except Exception, e:
+        print e
         print("FileGeodatabases can't be read by this driver: {0}.\nYou need GDAL/OGR >= 1.11".format("OpenFileGDB"))
 
 
