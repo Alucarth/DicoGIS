d2c4ceb8fdfe8b6dee0f653b4cbfba12f0f712f7
Julien <julien.moura@gmail.com>
2015-03-16 23:21:45 +0100

    just a few basic lines
    Former-commit-id: 5b7be67567b8089d45929c9c754e6dc5806aa971

--- test/test_GDAL_WMS.py
+++ test/test_GDAL_WMS.py
@@ -1,9 +1,13 @@
+# -*- coding: UTF-8 -*-
+#!/usr/bin/env python
+from __future__ import unicode_literals
+
 # gdalinfo "WMS:http://noisy.hq.isogeo.fr:6090/geoserver/wms?request=GetCapabilities&service=WMS&version=1.0.0"
 # gdalinfo "WMS:http://demo.opengeo.org/geoserver/wms?request=GetCapabilities&service=WMS&version=1.0.0"
 
-import gdal
+from osgeo import gdal
 
-wms = "http://noisy.hq.isogeo.fr:6090/geoserver/wms?request=GetCapabilities&service=WMS&version=1.0.0"
+wms = "http://demo.opengeo.org/geoserver/wms?request=GetCapabilities&service=WMS&version=1.0.0"
 flux = gdal.Open(wms)
 
 subdatasets = flux.GetMetadata("SUBDATASETS")
