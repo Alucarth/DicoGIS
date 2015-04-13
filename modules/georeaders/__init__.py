4971e736ce9f77acf593690cc1241871d4d9fb18
Julien <julien.moura@gmail.com>
2015-04-13 09:11:29 +0200

    minor fixes in stand-alone execution
    Former-commit-id: 5e250820915dfa72d9f3830369dfc20b5d43ee93

--- modules/georeaders/__init__.py
+++ modules/georeaders/__init__.py
@@ -5,6 +5,7 @@ from Infos_TAB import Read_TAB
 from Infos_GML import Read_GML
 from Infos_KML import Read_KML
 from Infos_GJSON import Read_GeoJSON
+from Infos_GXT import ReadGXT
 from Infos_PostGIS import Read_PostGIS
 from Infos_DXF import Read_DXF
 from Infos_GeoPDF import Read_GeoPDF
