4c889231c9361fa4899613ed49c5a91b4b057211
Julien <julien.moura@gmail.com>
2016-03-08 19:19:44 +0100

    finalize Check Norris
    Former-commit-id: 4622fd63791bbd186a0913fd2d9ed42c3e93b622

--- modules/utils/checknorris.py
+++ modules/utils/checknorris.py
@@ -10,14 +10,19 @@ from __future__ import (absolute_import, print_function, unicode_literals)
 #
 # Python:       2.7.x
 # Created:      22/12/2015
-# Updated:      10/01/2016
+# Updated:      10/03/2016
 # -----------------------------------------------------------------------------
 
 # #############################################################################
 # ###### Standard Libraries ########
 # ##################################
 
+import logging
+from os import environ as env, path, listdir
+import socket
 import sys
+from urllib2 import getproxies
+from urllib2 import build_opener, install_opener, ProxyHandler, urlopen
 
 # #############################################################################
 # ########## Classes ###############
@@ -31,47 +36,100 @@ class CheckNorris(object):
 
     # -- BEFORE ALL -----------------------------------------------------------
 
-    def __init__(self, proxy=None):
-        """ Instanciation
+    def __init__(self):
+        """ Check Norris welcomes you
         """
         super(CheckNorris, self).__init__()
 
-        # handling proxy parameters
-        # see: http://docs.python-requests.org/en/latest/user/advanced/#proxies
-        if proxy and type(proxy) is dict and 'http' in proxy.keys():
-            print("Proxy activated")
-            self.proxies = proxy
-        elif proxy and type(proxy) is not dict:
-            print("Proxy syntax error. Must be a dict { 'protocol': 'http://username:password@proxy_url:port' }.\
-                e.g.: {'http': 'http://martin:p4ssW0rde@10.1.68.1:5678',\
-                       'https': 'http://martin:p4ssW0rde@10.1.68.1:5678'}")
-            return
-        else:
-            self.proxies = {}
-            print("No proxy set. Use default configuration.")
-            pass
-
-    # -- API CONNECTION ------------------------------------------------------
+    # -- 1 method, 1 check ----------------------------------------------------
 
     def check_arcpy(self):
-        """ Chcks if arcpy and which verison is installed
+        """ Checks if arcpy and which version is installed
         """
         # 3rd party libraries
         try:
             import arcpy
-            print("Great! ArcGIS is well installed.")
+            esri_info = arcpy.GetInstallInfo()
+            return True, esri_info
         except ImportError:
-            print("ArcGIS isn't registered in the sys.path")
-            sys.path.append(r'C:\Program Files (x86)\ArcGIS\Desktop10.2\arcpy')
-            sys.path.append(r'C:\Program Files (x86)\ArcGIS\Desktop10.2\bin')
-            sys.path.append(r'C:\Program Files (x86)\ArcGIS\Desktop10.2\ArcToolbox\Scripts')
+            logging.info("ArcGIS isn't registered in the SYSPATH.\
+                          Trying to find it automatically.")
+            # checks if ArcGIS is installed
+            if not path.isdir(path.join(env.get("PROGRAMFILES(x86)"), "ArcGIS"))\
+               and not path.isdir(path.join(env.get("PROGRAMFILES"), "ArcGIS")):
+                logging.info("ArcGIS isn't installed on this computer.")
+                return False, "ArcGIS isn't installed on this computer."
+            else:
+                arcgis_path = path.join(env.get("PROGRAMFILES(x86)", "PROGRAMFILES"), "ArcGIS")
+                pass
+            logging.info("ArcGIS is installed but not well configured.")
+            # path to the last version of 10 branch
+            v = max([i[-1] for i in listdir(path.realpath(arcgis_path)) if "Desktop10" in i])
+            arcgis_path = path.join(arcgis_path, "Desktop10.{}".format(v))
+            # adding paths to the environment
+            sys.path.append(path.join(arcgis_path, "arcpy"))
+            sys.path.append(path.join(arcgis_path, "bin"))
+            sys.path.append(path.join(arcgis_path, "ArcToolbox\Scripts"))
             try:
-                from arcpy import env as enviro
-                from arcpy import ListDatasets, ListFeatureClasses, GetCount_management, ListFiles, ListFields, ListRasters, Describe
-                print("ArcGIS has been added to Python path and then imported.")
+                import arcpy
+                esri_info = arcpy.GetInstallInfo()
+                logging.info("ArcGIS configuration has been fixed.")
+                return True, esri_info
             except:
-                print("ArcGIS isn't installed on this computer")
-                sys.exit()
+                logging.info("ArcGIS automatic configuration failed.")
+                return False, "ArcGIS automatic configuration failed."
+        else:
+            logging.info("ArcGIS isn't installed on this computer.")
+            return False, "ArcGIS isn't installed on this computer."
+
+    def check_internet_connection(self, remote_server="www.google.com"):
+        """ Checks if an internet connection is operational
+        source: http://stackoverflow.com/a/20913928/2556577
+        """
+        try:
+            # see if we can resolve the host name -- tells us if there is
+            # a DNS listening
+            host = socket.gethostbyname(remote_server)
+            # connect to the host -- tells us if the host is actually
+            # reachable
+            socket.create_connection((host, 80), 2)
+            logging.info("Internet connection OK.")
+            return True
+        except:
+            logging.info("Internet connection failed.")
+            pass
+        # end of method
+        return False
+
+    def check_proxy(self, specific={}):
+        """ Checks if proxy settings are set on the OS
+        Returns:
+        -- 1 when direct connection works fine
+        -- 2 when direct connection fails and any proxy is set in the OS
+        -- 3 and settings when direct connection fails but a proxy is set
+        see: https://docs.python.org/2/library/urllib.html#urllib.getproxies
+        """
+        os_proxies = getproxies()
+        if len(os_proxies) == 0 and self.check_internet_connection:
+            logging.info("No proxy needed nor set. Direct connection works.")
+            return 1
+        elif len(os_proxies) == 0 and not self.check_internet_connection:
+            logging.error("Proxy not set in the OS. Needs to be specified")
+            return 2
+        else:
+            #
+            env['http_proxy'] = os_proxies.get("http")
+            env['https_proxy'] = os_proxies.get("https")
+            #
+            proxy = ProxyHandler({
+                                 'http': os_proxies.get("http"),
+                                 'https': os_proxies.get("https")
+                                 })
+            opener = build_opener(proxy)
+            install_opener(opener)
+            urlopen('http://www.google.com')
+            return 3, os_proxies
+
 
 # ##############################################################################
 # ##### Stand alone program ########
@@ -80,7 +138,16 @@ class CheckNorris(object):
 if __name__ == '__main__':
     """ standalone execution """
     # ------------ Specific imports ----------------
-    
+
     # ------------ Real start ----------------
     # instanciating the class
     checker = CheckNorris()
+
+    # checking arcpy installation
+    print("ArcPy: ", checker.check_arcpy())
+
+    # checking internet connection
+    print("Internet: ", checker.check_internet_connection())
+
+    # checking proxy settings
+    print("Proxy: ", checker.check_proxy())
