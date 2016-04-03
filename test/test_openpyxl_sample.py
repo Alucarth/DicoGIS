2ab0a2713c6eb8640c48d362a0e048f73ab36a40
Julien <julien.moura@gmail.com>
2016-04-03 16:41:06 +0200

    working on autofilter on xlsx
    Former-commit-id: d2f89a55512cf547aed04680f525ddf3e07ffa40

--- test/test_openpyxl_sample.py
+++ test/test_openpyxl_sample.py
@@ -1,15 +1,32 @@
 from openpyxl import Workbook
 from openpyxl.compat import range
 from openpyxl.cell import get_column_letter
+from openpyxl.styles import Style, Font, Alignment
+
+## styles
+header = Style(font=Font(bold=True))
+multilines = Style(alignment=Alignment(wrap_text=True))
+
+
 wb = Workbook()
-dest_filename = 'sample_workbook.xlsx'
+dest_filename = 'sample_workbook'
 ws1 = wb.active
 ws1.title = "range names"
 for row in range(1, 40):
-	ws1.append(range(600))
+    ws1.append(range(600))
+
+row1 = ws1.row_dimensions[1]
+row1.style = header
+
+wsprops = ws1.sheet_properties
+wsprops.tabColor = "1072BA"
+wsprops.filterMode = True
+
+ws1.auto_filter.add_filter_column(1, {}, True)
+ws1.auto_filter.add_filter_column(2, ["A1:A40"], True)
+
 
 ws2 = wb.create_sheet(title="Pi")
-
 ws2['F5'] = 3.14
 
 ws3 = wb.create_sheet(title="Data")
@@ -17,4 +34,46 @@ for row in range(10, 20):
     for col in range(27, 54):
         _ = ws3.cell(column=col, row=row, value="%s" % get_column_letter(col))
 print(ws3['AA10'].value)
-wb.save(filename = dest_filename)
+
+ws3.cell('A1').value = "Line 1\nLine 2\nLine 3"
+ws3.cell('A1').style = multilines
+
+
+ws4 = wb.create_sheet(title="Filtered data")
+
+data = [
+    ["Fruit", "Quantity"],
+    ["Kiwi", 3],
+    ["Grape", 15],
+    ["Apple", 3],
+    ["Peach", 3],
+    ["Pomegranate", 3],
+    ["Pear", 3],
+    ["Tangerine", 3],
+    ["Blueberry", 3],
+    ["Mango", 3],
+    ["Watermelon", 3],
+    ["Blackberry", 3],
+    ["Orange", 3],
+    ["Raspberry", 3],
+    ["Banana", 3]
+]
+
+for r in data:
+    ws4.append(r)
+
+# ws4.auto_filter.ref = "A1:B10"
+ws4.auto_filter.ref = "A1:{}{}".format(get_column_letter(ws4.max_column),
+                                       ws4.max_row)
+# print(ws4.auto_filter.ref)
+# ws4.auto_filter.add_filter_column(0, " ")
+# ws4.auto_filter.add_sort_condition("B2:B15")
+
+print(ws4.max_column)
+
+print(get_column_letter(ws4.max_column))
+
+try:
+    wb.save(filename=dest_filename + ".xlsx")
+except IOError:
+    wb.save(filename=dest_filename + "_2.xlsx")
