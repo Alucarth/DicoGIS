8682d8ad9fe607dddd4fccd7eb6c0986e30dbe6a
GeoJulien <julien.moura+dev@gmail.com>
2017-09-22 19:09:58 +0200

    remove future.install_aliases() references to finish #15

--- modules/georeaders/Infos_Spatialite.py
+++ modules/georeaders/Infos_Spatialite.py
@@ -1,7 +1,6 @@
 # -*- coding: UTF-8 -*-
 #!/usr/bin/env python
-from __future__ import unicode_literals
-from __future__ import print_function
+from __future__ import (absolute_import, print_function, unicode_literals)
 
 # ----------------------------------------------------------------------------
 # Name:         InfosSpatialite
